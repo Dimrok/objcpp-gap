ff8de379157a9448e39cb7625dc18218cbefd491
Christopher Crone <christopher.crone@infinit.io>
2014-11-25 15:53:43 +0100

    TemporaryFileManager: Handle transfers that're bigger than the mirror size better.

--- Gap/InfinitTemporaryFileManager.mm
+++ Gap/InfinitTemporaryFileManager.mm
@@ -11,6 +11,7 @@
 #import "InfinitManagedFiles.h"
 #import "InfinitLinkTransactionManager.h"
 #import "InfinitPeerTransactionManager.h"
+#import "InfinitStateManager.h"
 
 #undef check
 #import <elle/log.hh>
@@ -25,6 +26,7 @@ static InfinitTemporaryFileManager* _instance = nil;
   NSMutableDictionary* _files_map;
   NSMutableDictionary* _transaction_map;
   NSString* _managed_root;
+  uint64_t _max_mirror_size;
 }
 
 - (id)init
@@ -32,16 +34,18 @@ static InfinitTemporaryFileManager* _instance = nil;
   NSCAssert(_instance == nil, @"Use the sharedInstance");
   if (self = [super init])
   {
+    _max_mirror_size = [[InfinitStateManager sharedInstance] max_mirror_size];
     _files_map = [NSMutableDictionary dictionary];
     _transaction_map = [NSMutableDictionary dictionary];
     _managed_root = [NSTemporaryDirectory() stringByAppendingPathComponent:@"managed_files"];
+    [self _deleteFiles:@[_managed_root]];
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(transactionUpdated:)
+                                             selector:@selector(_linkTransactionUpdated:)
                                                  name:INFINIT_LINK_TRANSACTION_STATUS_NOTIFICATION
                                                object:nil];
 
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(transactionUpdated:)
+                                             selector:@selector(_peerTransactionUpdated:)
                                                  name:INFINIT_PEER_TRANSACTION_STATUS_NOTIFICATION
                                                object:nil];
   }
@@ -63,14 +67,54 @@ static InfinitTemporaryFileManager* _instance = nil;
 
 #pragma mark - Transaction Callback
 
-- (void)transactionUpdated:(NSNotification*)notification
+- (BOOL)_transactionFilesNeededForStatus:(gap_TransactionStatus)status
+{
+  switch (status)
+  {
+    case gap_transaction_canceled:
+    case gap_transaction_cloud_buffered:
+    case gap_transaction_deleted:
+    case gap_transaction_failed:
+    case gap_transaction_finished:
+    case gap_transaction_rejected:
+    case gap_transaction_on_other_device:
+      return NO;
+
+    default:
+      return YES;
+  }
+}
+
+- (void)_linkTransactionUpdated:(NSNotification*)notification
 {
   NSNumber* id_ = notification.userInfo[@"id"];
   InfinitManagedFiles* managed_files = [_transaction_map objectForKey:id_];
   if (managed_files == nil)
     return;
-  [self deleteManagedFiles:managed_files.uuid];
-  [_transaction_map removeObjectForKey:id_];
+  InfinitLinkTransaction* transaction =
+    [[InfinitLinkTransactionManager sharedInstance] transactionWithId:id_];
+  if (managed_files.total_size.unsignedIntegerValue < _max_mirror_size ||
+      ![self _transactionFilesNeededForStatus:transaction.status])
+  {
+    [self deleteManagedFiles:managed_files.uuid];
+    [_transaction_map removeObjectForKey:id_];
+  }
+}
+
+- (void)_peerTransactionUpdated:(NSNotification*)notification
+{
+  NSNumber* id_ = notification.userInfo[@"id"];
+  InfinitManagedFiles* managed_files = [_transaction_map objectForKey:id_];
+  if (managed_files == nil)
+    return;
+  InfinitPeerTransaction* transaction =
+    [[InfinitPeerTransactionManager sharedInstance] transactionWithId:id_];
+  if (managed_files.total_size.unsignedIntegerValue < _max_mirror_size ||
+      ![self _transactionFilesNeededForStatus:transaction.status])
+  {
+    [self deleteManagedFiles:managed_files.uuid];
+    [_transaction_map removeObjectForKey:id_];
+  }
 }
 
 #pragma mark - Public
@@ -105,7 +149,6 @@ static InfinitTemporaryFileManager* _instance = nil;
              self.description.UTF8String, uuid.UTF8String);
     return;
   }
-  NSLog(@"xxx %@ has transaction_id: %@", uuid, transaction_id);
   [_transaction_map setObject:managed_files forKey:transaction_id];
 }
 
@@ -127,8 +170,10 @@ static InfinitTemporaryFileManager* _instance = nil;
   }
   else
   {
-    [managed_files.managed_paths addObjectsFromArray:files];
+    [managed_files.managed_paths addObjectsFromArray:[self _moveFiles:files
+                                                               toPath:managed_files.root_dir]];
   }
+  managed_files.total_size = [self _folderSize:managed_files.root_dir];
 }
 
 - (void)removeFiles:(NSArray*)files
@@ -146,6 +191,7 @@ static InfinitTemporaryFileManager* _instance = nil;
     [managed_files.managed_paths removeObject:file];
   }
   [self _deleteFiles:files];
+  managed_files.total_size = [self _folderSize:managed_files.root_dir];
 }
 
 - (void)deleteManagedFiles:(NSString*)uuid
@@ -158,9 +204,7 @@ static InfinitTemporaryFileManager* _instance = nil;
     return;
   }
   [_files_map removeObjectForKey:uuid];
-  [self _deleteFiles:managed_files.managed_paths.array];
-  if ([self _pathExists:managed_files.root_dir])
-    [self _deleteFiles:@[managed_files.root_dir]];
+  [self _deleteFiles:@[managed_files.root_dir]];
 }
 
 #pragma mark - Helpers
@@ -212,6 +256,32 @@ static InfinitTemporaryFileManager* _instance = nil;
   return res;
 }
 
+- (NSArray*)_moveFiles:(NSArray*)files
+                toPath:(NSString*)path
+{
+  if (![self _pathExists:path])
+    [self _createDirectoryAtPath:path];
+  NSError* error;
+  NSString* move_path;
+  BOOL success;
+  NSMutableArray* res = [NSMutableArray array];
+  for (NSString* file in files)
+  {
+    move_path = [path stringByAppendingPathComponent:file.lastPathComponent];
+    success = [[NSFileManager defaultManager] moveItemAtPath:file toPath:move_path error:&error];
+    if (success)
+    {
+      [res addObject:move_path];
+    }
+    else
+    {
+      ELLE_ERR("%s: unable to move %s to %s: %s", self.description.UTF8String,
+               file.UTF8String, path.UTF8String, error.description.UTF8String);
+    }
+  }
+  return res;
+}
+
 - (void)_deleteFiles:(NSArray*)files
 {
   NSError* error;
@@ -224,8 +294,28 @@ static InfinitTemporaryFileManager* _instance = nil;
       ELLE_WARN("%s: unable to remove %s: %s", self.description.UTF8String,
                 file.UTF8String, error.description.UTF8String);
     }
-    NSLog(@"xxx deleted: %@", file);
   }
+}
+
+- (NSNumber*)_folderSize:(NSString*)path
+{
+  if (![self _pathExists:path])
+    return @0;
+  NSURL* folder_url = [NSURL fileURLWithPath:path];
+  NSNumber* temp_size = @0;
+  NSUInteger res = 0;
+  NSDirectoryEnumerator* dir_enum =
+    [[NSFileManager defaultManager] enumeratorAtURL:folder_url
+                         includingPropertiesForKeys:@[NSURLTotalFileAllocatedSizeKey,
+                                                      NSURLIsDirectoryKey]
+                                            options:NSDirectoryEnumerationSkipsHiddenFiles
+                                       errorHandler:nil];
+  for (NSURL* file_url in dir_enum)
+  {
+    [file_url getResourceValue:&temp_size forKey:NSURLFileSizeKey error:NULL];
+    res += temp_size.unsignedIntegerValue;
+  }
+  return [NSNumber numberWithUnsignedInteger:res];
 }
 
 @end
