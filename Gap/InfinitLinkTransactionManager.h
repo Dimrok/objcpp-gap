717419cf7c059447edb8130f7c5fa6bf18898004
Christopher Crone <christopher.crone@infinit.io>
2014-11-26 09:59:46 +0100

    LinkTransactionManager: Add documentation.

--- Gap/InfinitLinkTransactionManager.h
+++ Gap/InfinitLinkTransactionManager.h
@@ -10,9 +10,24 @@
 
 #import "InfinitLinkTransaction.h"
 
+/** Notification sent when there is a new link transaction.
+ Contains a dictionary with the transaction "id".
+ */
 #define INFINIT_NEW_LINK_TRANSACTION_NOTIFICATION     @"INFINIT_NEW_LINK_TRANSACTION_NOTIFICATION"
+
+/** Notification sent when an existing link transaction's data has changed. (e.g.: click count).
+ Contains a dictionary with the transaction "id".
+*/
 #define INFINIT_LINK_TRANSACTION_DATA_NOTIFICATION    @"INFINIT_LINK_TRANSACTION_DATA_NOTIFICATION"
+
+/** Notification sent when the status of an existing link transaction is updated.
+ Contains a dictionary with the transaction "id".
+*/
 #define INFINIT_LINK_TRANSACTION_STATUS_NOTIFICATION  @"INFINIT_LINK_TRANSACTION_STATUS_NOTIFICATION"
+
+/** Notification sent when an existing link transaction has been deleted.
+ Contains a dictionary with the transaction "id".
+*/
 #define INFINIT_LINK_TRANSACTION_DELETED_NOTIFICATION @"INFINIT_LINK_TRANSACTION_DELETED_NOTIFICATION"
 
 @interface InfinitLinkTransactionManager : NSObject
