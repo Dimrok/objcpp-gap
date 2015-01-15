c9bd6017816be9c9eca6cff7f3764666cf5f775c
Christopher Crone <christopher.crone@infinit.io>
2015-01-15 20:11:48 +0100

    Refactor utilities.

--- Gap/NSString+email.m
+++ Gap/NSString+email.m
@@ -1,17 +1,19 @@
 //
-//  InfinitUtilities.m
+//  NSString+email.m
 //  Gap
 //
-//  Created by Christopher Crone on 13/11/14.
+//  Created by Christopher Crone on 15/01/15.
 //
 //
 
-#import "InfinitUtilities.h"
+#import "NSString+email.h"
 
-@implementation InfinitUtilities
+@implementation NSString (email)
 
-+ (BOOL)stringIsEmail:(NSString*)string
+- (BOOL)isEmail
 {
+  if (self.length == 0)
+    return NO;
   NSString* email_regex =
     @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
     @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
@@ -21,7 +23,7 @@
     @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
     @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
   NSPredicate* email_test = [NSPredicate predicateWithFormat:@"SELF MATCHES[c] %@", email_regex];
-  return [email_test evaluateWithObject:string];
+  return [email_test evaluateWithObject:self];
 }
 
 @end
