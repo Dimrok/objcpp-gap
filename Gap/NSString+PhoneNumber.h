6642d22b5cf4b3f4ea182e8cc7a7ce82dbc73d28
Christopher Crone <christopher.crone@infinit.io>
2015-03-07 18:36:03 +0100

    NSString: Add helper for checking if string is phone number.

---
+++ Gap/NSString+PhoneNumber.h
@@ -0,0 +1,15 @@
+//
+//  NSString+PhoneNumber.h
+//  Gap
+//
+//  Created by Christopher Crone on 07/03/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PhoneNumber)
+
+@property (nonatomic, readonly) BOOL isPhoneNumber;
+
+@end
---
+++ Gap/NSString+PhoneNumber.m
@@ -0,0 +1,29 @@
+//
+//  NSString+PhoneNumber.m
+//  Gap
+//
+//  Created by Christopher Crone on 07/03/15.
+//
+//
+
+#import "NSString+PhoneNumber.h"
+
+@implementation NSString (PhoneNumber)
+
+- (BOOL)isPhoneNumber
+{
+  NSError* error = nil;
+  NSDataDetector* detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypePhoneNumber
+                                                             error:&error];
+  if (error)
+    return NO;
+  NSRange range = NSMakeRange(0, self.length);
+  NSArray* matches = [detector matchesInString:self options:0 range:range];
+  if (matches.count != 1)
+    return NO;
+  if ([matches[0] length] == self.length)
+    return YES;
+  return NO;
+}
+
+@end
