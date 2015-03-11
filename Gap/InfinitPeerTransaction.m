dc31d4a88702ec16701ce49a8af2498589f1192a
Christopher Crone <christopher.crone@infinit.io>
2015-03-12 00:44:49 +0100

    PeerTransaction: Improve description.

--- Gap/InfinitPeerTransaction.m
+++ Gap/InfinitPeerTransaction.m
@@ -152,13 +152,14 @@ recipient_device:(NSString*)recipient_device_id
 
 - (NSString*)description
 {
-  return [NSString stringWithFormat:@"<%@: "
+  return [NSString stringWithFormat:@"<%@ (%@): "
           "status: %@\n"
           "sender: %@\n"
           "sender device: %@\n"
           "recipient: %@\n"
-          "recipient device: %@>",
-          self.id_, self.status_text, self.sender.id_, self.sender_device_id, self.recipient.id_, self.recipient_device];
+          "recipient device: %@"
+          "%@archived>",
+          self.meta_id, self.id_, self.status_text, self.sender.id_, self.sender_device_id, self.recipient.id_, self.recipient_device, self.archived ? @"" : @"un"];
 }
 
 @end
