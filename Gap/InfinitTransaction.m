2559add9e09febe8e9d2793860a064696478d395
Christopher Crone <christopher.crone@infinit.io>
2015-03-12 10:17:42 +0100

    Transaction: Restore cloud_buffered as done state.

--- Gap/InfinitTransaction.m
+++ Gap/InfinitTransaction.m
@@ -58,6 +58,7 @@ sender_device_id:(NSString*)sender_device_id
 {
   switch (self.status)
   {
+    case gap_transaction_cloud_buffered:
     case gap_transaction_finished:
     case gap_transaction_failed:
     case gap_transaction_canceled:
