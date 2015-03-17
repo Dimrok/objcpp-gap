2e9cea6b4484b28cbff220d67b459c1b610d41ca
Christopher Crone <christopher.crone@infinit.io>
2015-03-17 17:30:52 +0100

    Color: Add to Gap so that it can be shared by iOS and OS X.

---
+++ Gap/InfinitColor.h
@@ -0,0 +1,39 @@
+//
+//  InfinitColor.h
+//  Infinit
+//
+//  Created by Christopher Crone on 08/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#if TARGET_OS_IPHONE
+# import <UIKit/UIKit.h>
+# define INFINIT_COLOR UIColor
+#else
+# import <AppKit/AppKit.h>
+# define INFINIT_COLOR NSColor
+#endif
+
+typedef NS_ENUM(NSUInteger, InfinitPaletteColors)
+{
+  InfinitPaletteColorBurntSienna, // 242, 94,  90
+  InfinitPaletteColorShamRock,    // 43,  190, 189
+  InfinitPaletteColorChicago,     // 100, 100, 90
+  InfinitPaletteColorLightGray,   // 243, 243, 243
+  InfinitPaletteColorSendBlack,   // 46,  46,  46
+};
+
+@interface InfinitColor : NSObject
+
++ (INFINIT_COLOR*)colorWithGray:(NSUInteger)gray;
++ (INFINIT_COLOR*)colorWithGray:(NSUInteger)gray alpha:(CGFloat)alpha;
+
++ (INFINIT_COLOR*)colorWithRed:(NSUInteger)red green:(NSUInteger)green blue:(NSUInteger)blue;
++ (INFINIT_COLOR*)colorWithRed:(NSUInteger)red green:(NSUInteger)green blue:(NSUInteger)blue
+                         alpha:(CGFloat)alpha;
+
++ (INFINIT_COLOR*)colorFromPalette:(InfinitPaletteColors)color;
++ (INFINIT_COLOR*)colorFromPalette:(InfinitPaletteColors)color alpha:(CGFloat)alpha;
+
+@end
---
+++ Gap/InfinitColor.m
@@ -0,0 +1,60 @@
+//
+//  InfinitColor.m
+//  Infinit
+//
+//  Created by Christopher Crone on 08/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitColor.h"
+
+@implementation InfinitColor
+
++ (INFINIT_COLOR*)colorWithGray:(NSUInteger)gray
+{
+  return [InfinitColor colorWithGray:gray alpha:1.0];
+}
+
++ (INFINIT_COLOR*)colorWithGray:(NSUInteger)gray alpha:(CGFloat)alpha
+{
+  return [InfinitColor colorWithRed:gray green:gray blue:gray alpha:alpha];
+}
+
++ (INFINIT_COLOR*)colorWithRed:(NSUInteger)red green:(NSUInteger)green blue:(NSUInteger)blue
+{
+  return [InfinitColor colorWithRed:red green:green blue:blue alpha:1.0];
+}
+
++ (INFINIT_COLOR*)colorWithRed:(NSUInteger)red green:(NSUInteger)green blue:(NSUInteger)blue
+                         alpha:(CGFloat)alpha
+{
+  return [INFINIT_COLOR colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:alpha];
+}
+
++ (INFINIT_COLOR*)colorFromPalette:(InfinitPaletteColors)color
+{
+  return [InfinitColor colorFromPalette:color alpha:1.0];
+}
+
++ (INFINIT_COLOR*)colorFromPalette:(InfinitPaletteColors)color
+                             alpha:(CGFloat)alpha
+{
+  switch (color)
+  {
+    case InfinitPaletteColorBurntSienna:
+      return [InfinitColor colorWithRed:242 green:94 blue:90 alpha:alpha];
+    case InfinitPaletteColorShamRock:
+      return [InfinitColor colorWithRed:43 green:190 blue:189 alpha:alpha];
+    case InfinitPaletteColorChicago:
+      return [InfinitColor colorWithRed:100 green:100 blue:90 alpha:alpha];
+    case InfinitPaletteColorLightGray:
+      return [InfinitColor colorWithGray:243 alpha:alpha];
+    case InfinitPaletteColorSendBlack:
+      return [InfinitColor colorWithGray:46 alpha:alpha];
+
+    default:
+      return [InfinitColor colorWithRed:0 green:0 blue:0 alpha:alpha];
+  }
+}
+
+@end
