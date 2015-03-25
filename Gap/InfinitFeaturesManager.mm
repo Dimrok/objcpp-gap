50b35251a9e35e47b70eeecffe16fcf51db8829a
Christopher Crone <christopher.crone@infinit.io>
2015-03-25 10:07:12 +0100

    FeaturesManager: Use GCD to ensure only one instance can be created. Ensure features are refreshed when model is cleared.

--- Gap/InfinitFeaturesManager.mm
+++ Gap/InfinitFeaturesManager.mm
@@ -15,6 +15,7 @@
 ELLE_LOG_COMPONENT("Gap-ObjC++.FeaturesManager");
 
 static InfinitFeaturesManager* _instance = nil;
+static dispatch_once_t _instance_token = 0;
 
 @implementation InfinitFeaturesManager
 {
@@ -22,26 +23,48 @@ static InfinitFeaturesManager* _instance = nil;
   NSDictionary* _features;
 }
 
+#pragma mark - Init
+
 - (id)init
 {
   NSCAssert(_instance == nil, @"Use the sharedInstance");
   if (self = [super init])
   {
     _features = [[[InfinitStateManager sharedInstance] features] copy];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(clearModel)
+                                                 name:INFINIT_CLEAR_MODEL_NOTIFICATION
+                                               object:nil];
   }
   return self;
 }
 
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 + (instancetype)sharedInstance
 {
-  if (_instance == nil)
+  dispatch_once(&_instance_token, ^{
     _instance = [[InfinitFeaturesManager alloc] init];
+  });
   return _instance;
 }
+
+#pragma mark - General
 
 - (NSDictionary*)features
 {
   return _features;
+}
+
+#pragma mark - Clear Model
+
+- (void)clearModel
+{
+  _instance = nil;
+  _instance_token = 0;
 }
 
 @end
