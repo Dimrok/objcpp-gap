11a7e8538f5d5b78d87aae5c48dd9708ad010908
Christopher Crone <christopher.crone@infinit.io>
2015-03-30 15:21:09 +0200

    ManagedFiles: Add NSCoding support. Improve description.

--- Gap/InfinitManagedFiles.h
+++ Gap/InfinitManagedFiles.h
@@ -8,12 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
-@interface InfinitManagedFiles : NSObject
+@interface InfinitManagedFiles : NSObject <NSCoding>
 
 @property (nonatomic, readonly) NSString* uuid;
 @property (nonatomic, readwrite) NSMutableOrderedSet* managed_paths;
 @property (nonatomic, readwrite) NSString* root_dir;
-@property (nonatomic, readwrite) NSNumber* transaction_id;
 @property (nonatomic, readwrite) NSNumber* total_size;
 @property (nonatomic, readwrite) NSMutableDictionary* asset_map;
 
--- Gap/InfinitManagedFiles.m
+++ Gap/InfinitManagedFiles.m
@@ -22,4 +22,46 @@
   return self;
 }
 
+#pragma mark - NSCoding
+
+- (id)initWithCoder:(NSCoder*)decoder
+{
+  if (self = [super init])
+  {
+    _uuid = [decoder decodeObjectForKey:@"uuid"];
+    _managed_paths =
+      [NSMutableOrderedSet orderedSetWithArray:[decoder decodeObjectForKey:@"managed_paths"]];
+    _root_dir = [decoder decodeObjectForKey:@"root_dir"];
+    _total_size = [decoder decodeObjectForKey:@"total_size"];
+    _asset_map = [decoder decodeObjectForKey:@"asset_map"];
+  }
+  return self;
+}
+
+- (void)encodeWithCoder:(NSCoder*)encoder
+{
+  [encoder encodeObject:self.uuid forKey:@"uuid"];
+  [encoder encodeObject:self.managed_paths.array forKey:@"managed_paths"];
+  [encoder encodeObject:self.root_dir forKey:@"root_dir"];
+  [encoder encodeObject:self.total_size forKey:@"total_size"];
+  [encoder encodeObject:self.asset_map forKey:@"asset_map"];
+}
+
+#pragma mark - NSObject
+
+- (BOOL)isEqual:(id)object
+{
+  if (![object isKindOfClass:InfinitManagedFiles.class])
+    return NO;
+  InfinitManagedFiles* other = (InfinitManagedFiles*)object;
+  if ([self.uuid isEqualToString:other.uuid])
+    return YES;
+  return NO;
+}
+
+- (NSString*)description
+{
+  return [NSString stringWithFormat:@"<InfinitManagedFiles %p: %@>", self, self.uuid];
+}
+
 @end
