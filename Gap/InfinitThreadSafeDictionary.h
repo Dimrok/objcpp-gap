aeb20febe6469eb4af9a2ea1e01a207b72604926
Christopher Crone <christopher.crone@infinit.io>
2015-05-01 12:55:20 +0200

    ThreadSafeDictionary: Update.

--- Gap/InfinitThreadSafeDictionary.h
+++ Gap/InfinitThreadSafeDictionary.h
@@ -10,13 +10,14 @@
 
 @interface InfinitThreadSafeDictionary : NSObject
 
-- (instancetype)initWithName:(NSString*)name;
++ (instancetype)initWithName:(NSString*)name;
 
 - (NSArray*)allKeys;
 - (NSArray*)allValues;
 
 - (id)objectForKey:(id)key;
 - (id)objectForKeyedSubscript:(id)key;
+- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL* stop))block;
 
 - (void)setObject:(id)obj forKey:(id<NSCopying>)key;
 - (void)setObject:(id)obj forKeyedSubscript:(id <NSCopying>)key;
--- Gap/InfinitThreadSafeDictionary.m
+++ Gap/InfinitThreadSafeDictionary.m
@@ -10,13 +10,14 @@
 
 @interface InfinitThreadSafeDictionary ()
 
-@property (readonly, strong) NSMutableDictionary* dictionary;
-#if OS_OBJECT_HAVE_OBJC_SUPPORT
-@property (readonly, strong) dispatch_queue_t queue;
-#else
-@property (readonly, assign) dispatch_queue_t queue;
-#endif
+@property (nonatomic, readonly) NSMutableDictionary* dictionary;
 @property (atomic, readonly) BOOL finalize;
+#if OS_OBJECT_HAVE_OBJC_SUPPORT
+@property (nonatomic, readonly) dispatch_queue_t queue;
+#else
+@property (nonatomic, assign) dispatch_queue_t queue;
+#endif
+@property (nonatomic, readonly) NSString* queue_name;
 
 @end
 
@@ -24,14 +25,20 @@
 
 - (instancetype)initWithName:(NSString*)name
 {
+  NSCAssert(name.length, @"Ensure name has length.");
   if (self = [super init])
   {
     _finalize = NO;
-    NSString* queue_name = [NSString stringWithFormat:@"io.Infinit.ThreadSafeDictionary-%@", name];
-    _queue = dispatch_queue_create(queue_name.UTF8String, DISPATCH_QUEUE_SERIAL);
+    _queue_name = [NSString stringWithFormat:@"io.Infinit.ThreadSafeDictionary-%@", name];
+    _queue = dispatch_queue_create(self.queue_name.UTF8String, DISPATCH_QUEUE_SERIAL);
     _dictionary = [[NSMutableDictionary alloc] init];
   }
   return self;
+}
+
++ (instancetype)initWithName:(NSString*)name
+{
+  return [[InfinitThreadSafeDictionary alloc] initWithName:name];
 }
 
 - (void)dealloc
@@ -45,10 +52,10 @@
 - (NSArray*)allKeys
 {
   __block NSArray* res = nil;
+  if (self.finalize)
+    return res;
   dispatch_barrier_sync(self.queue, ^
   {
-    if (self.finalize)
-      return;
     res = self.dictionary.allKeys;
   });
   return res;
@@ -57,10 +64,10 @@
 - (NSArray*)allValues
 {
   __block NSArray* res = nil;
+  if (self.finalize)
+    return res;
   dispatch_barrier_sync(self.queue, ^
   {
-    if (self.finalize)
-      return;
     res = self.dictionary.allValues;
   });
   return res;
@@ -69,10 +76,10 @@
 - (id)objectForKey:(id)key
 {
   __block id res = nil;
+  if (self.finalize)
+    return res;
   dispatch_barrier_sync(self.queue, ^
   {
-    if (self.finalize)
-      return;
     res = [self.dictionary objectForKey:key];
   });
   return res;
@@ -81,13 +88,23 @@
 - (id)objectForKeyedSubscript:(id)key
 {
   __block id res = nil;
+  if (self.finalize)
+    return res;
   dispatch_barrier_sync(self.queue, ^
   {
-    if (self.finalize)
-      return;
     res = self.dictionary[key];
   });
   return res;
+}
+
+- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL* stop))block
+{
+  if (self.finalize)
+    return;
+  dispatch_sync(self.queue, ^
+  {
+    [self.dictionary enumerateKeysAndObjectsUsingBlock:block];
+  });
 }
 
 #pragma mark - Setters
@@ -95,10 +112,10 @@
 - (void)setObject:(id)obj
            forKey:(id<NSCopying>)key
 {
+  if (self.finalize)
+    return;
   dispatch_barrier_async(self.queue, ^
   {
-    if (self.finalize)
-      return;
     [self.dictionary setObject:obj forKey:key];
   });
 }
@@ -106,32 +123,39 @@
 - (void)setObject:(id)obj
 forKeyedSubscript:(id <NSCopying>)key
 {
+  if (self.finalize)
+    return;
   dispatch_barrier_async(self.queue, ^
   {
-    if (self.finalize)
-      return;
     self.dictionary[key] = obj;
   });
 }
 
 - (void)removeObjectForKey:(id)key
 {
+  if (self.finalize)
+    return;
   dispatch_barrier_async(self.queue, ^
   {
-    if (self.finalize)
-      return;
     [self.dictionary removeObjectForKey:key];
   });
 }
 
 - (void)removeAllObjects
 {
+  if (self.finalize)
+    return;
   dispatch_barrier_sync(self.queue, ^
   {
-    if (self.finalize)
-      return;
     [self.dictionary removeAllObjects];
   });
+}
+
+#pragma mark - NSObject
+
+- (NSString*)description
+{
+  return [NSString stringWithFormat:@"<%p %@: %@>", self, self.queue_name, self.dictionary];
 }
 
 @end
