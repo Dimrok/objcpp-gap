07dca7890d925453936b9f6a0c2b796f1beab2c2
Christopher Crone <christopher.crone@infinit.io>
2015-05-19 18:37:21 +0200

    GhostCodeManager: Rework for new gap interface.

---
+++ Gap/InfinitGhostCodeManager.h
@@ -0,0 +1,36 @@
+//
+//  InfinitGhostCodeManager.h
+//  Gap
+//
+//  Created by Christopher Crone on 19/05/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^InfinitGhostCodeUsedBlock)(NSString* code, BOOL success, NSString* reason);
+
+@interface InfinitGhostCodeManager : NSObject
+
++ (instancetype)sharedInstance;
+
+/** Set ghost code.
+ Passes the ghost code to State to manage. The callback will be called when the code is actually
+ used.
+ @param code
+  Ghost code to pass.
+ @param was_link
+  If the code was received via a link (i.e.: Not manually entered).
+ @param completion_block
+  Block to run on use of the ghost code. Can be nil.
+ */
+- (void)setCode:(NSString*)code
+        wasLink:(BOOL)was_link
+completionBlock:(InfinitGhostCodeUsedBlock)completion_block;
+
+#pragma mark - State Manager Callback
+- (void)ghostCodeUsed:(NSString*)code
+              success:(BOOL)success
+               reason:(NSString*)reason;
+
+@end
---
+++ Gap/InfinitGhostCodeManager.mm
@@ -0,0 +1,82 @@
+//
+//  InfinitGhostCodeManager.m
+//  Gap
+//
+//  Created by Christopher Crone on 19/05/15.
+//
+//
+
+#import "InfinitGhostCodeManager.h"
+
+#import "InfinitStateManager.h"
+#import "InfinitURLParser.h"
+
+#undef check
+#import <elle/log.hh>
+
+ELLE_LOG_COMPONENT("Gap-ObjC++.GhostCodeManager");
+
+@interface InfinitGhostCodeManager ()
+
+@property (atomic, readonly) InfinitGhostCodeUsedBlock code_used_block;
+
+@end
+
+static InfinitGhostCodeManager* _instance = nil;
+static dispatch_once_t _instance_token = 0;
+
+@implementation InfinitGhostCodeManager
+
+#pragma mark - Init
+
+- (id)init
+{
+  NSCAssert(_instance == nil, @"Use sharedInstance.");
+  if (self = [super init])
+  {}
+  return self;
+}
+
++ (instancetype)sharedInstance
+{
+  dispatch_once(&_instance_token, ^
+  {
+    _instance = [[InfinitGhostCodeManager alloc] init];
+  });
+  return _instance;
+}
+
+#pragma mark - Code Handling
+
+- (void)setCode:(NSString*)code
+        wasLink:(BOOL)was_link
+completionBlock:(InfinitGhostCodeUsedBlock)completion_block
+{
+  if (self.code_used_block)
+  {
+    ELLE_WARN("%s: overwriting ghost code used block", self.description.UTF8String);
+  }
+  _code_used_block = completion_block;
+  [[InfinitStateManager sharedInstance] useGhostCode:code wasLink:was_link];
+}
+
+#pragma mark - State Manager Callback
+
+- (void)ghostCodeUsed:(NSString*)code
+              success:(BOOL)success
+               reason:(NSString*)reason
+{
+  dispatch_async(dispatch_get_main_queue(), ^
+  {
+    if (self.code_used_block)
+    {
+      ELLE_TRACE("%s: ghost code used, run callback", self.description.UTF8String);
+      self.code_used_block(code, success, reason);
+      _code_used_block = nil;
+      return;
+    }
+    ELLE_TRACE("%s: ghost code used, no callback to run", self.description.UTF8String);
+  });
+}
+
+@end
