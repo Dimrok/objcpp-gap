32b9d829244f27bd0510decdcad17a275ef51678
Christopher Crone <christopher.crone@infinit.io>
2015-06-22 17:48:18 +0200

    ManagedFiles: Update for earlier fetching of gallery items.

--- Gap/InfinitManagedFiles.h
+++ Gap/InfinitManagedFiles.h
@@ -10,10 +10,29 @@
 
 @interface InfinitManagedFiles : NSObject <NSCoding>
 
-@property (nonatomic, readonly) NSString* uuid;
-@property (nonatomic, readwrite) NSMutableOrderedSet* managed_paths;
-@property (nonatomic, readwrite) NSString* root_dir;
-@property (nonatomic, readwrite) NSNumber* total_size;
+/// Asset URLs that have been converted to files.
 @property (nonatomic, readwrite) NSMutableDictionary* asset_map;
+/// If the files were copied to a temporary location.
+@property (nonatomic, readwrite) BOOL copied;
+/// If the files are currently copying.
+@property (nonatomic, readwrite) BOOL copying;
+/// Callback to run when finished copying.
+@property (nonatomic, assign) void (^done_copying_block)();
+/// File count.
+@property (nonatomic, readonly) NSUInteger file_count;
+/// Paths to files.
+@property (nonatomic, readwrite) NSMutableOrderedSet* managed_paths;
+/// Assets that should not be sent but were added at some point.
+@property (nonatomic, readonly) NSMutableSet* remove_assets;
+/// Root directory where files are stored if they were copied.
+@property (nonatomic, readwrite) NSString* root_dir;
+/// If the managed files are in the process of being sent.
+@property (atomic, readwrite) BOOL sending;
+/// Array of sorted paths.
+@property (nonatomic, readonly) NSArray* sorted_paths;
+/// Total size of the files.
+@property (nonatomic, readwrite) uint64_t total_size;
+/// Unique identifier for the files.
+@property (nonatomic, readonly) NSString* uuid;
 
 @end
--- Gap/InfinitManagedFiles.m
+++ Gap/InfinitManagedFiles.m
@@ -10,6 +10,10 @@
 
 @implementation InfinitManagedFiles
 
+@synthesize copying = _copying;
+
+#pragma mark - Init
+
 - (id)init
 {
   if (self = [super init])
@@ -17,9 +21,57 @@
     _uuid = [NSUUID UUID].UUIDString;
     _managed_paths = [NSMutableOrderedSet orderedSet];
     _asset_map = [NSMutableDictionary dictionary];
-    _total_size = @0;
+    _remove_assets = [NSMutableSet set];
   }
   return self;
+}
+
+#pragma mark - Public
+
+- (NSUInteger)file_count
+{
+  return self.managed_paths.count;
+}
+
+- (BOOL)copying
+{
+  @synchronized(self)
+  {
+    return _copying;
+  }
+}
+
+- (void)setCopying:(BOOL)copying
+{
+  @synchronized(self)
+  {
+    _copying = copying;
+    if (!copying)
+    {
+      for (id asset_ref in self.remove_assets)
+      {
+        NSString* path = [self.asset_map objectForKey:asset_ref];
+        [self.asset_map removeObjectForKey:asset_ref];
+        if (path)
+        {
+          [self.managed_paths removeObject:path];
+          [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
+        }
+      }
+      if (self.done_copying_block)
+      {
+        dispatch_sync(dispatch_get_main_queue(), ^
+        {
+          self.done_copying_block();
+        });
+      }
+    }
+  }
+}
+
+- (NSArray*)sorted_paths
+{
+  return [self.managed_paths.array sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
 }
 
 #pragma mark - NSCoding
@@ -32,7 +84,7 @@
     _managed_paths =
       [NSMutableOrderedSet orderedSetWithArray:[decoder decodeObjectForKey:@"managed_paths"]];
     _root_dir = [decoder decodeObjectForKey:@"root_dir"];
-    _total_size = [decoder decodeObjectForKey:@"total_size"];
+    _total_size = [[decoder decodeObjectForKey:@"total_size"] unsignedIntegerValue];
     _asset_map = [decoder decodeObjectForKey:@"asset_map"];
   }
   return self;
@@ -43,7 +95,7 @@
   [encoder encodeObject:self.uuid forKey:@"uuid"];
   [encoder encodeObject:self.managed_paths.array forKey:@"managed_paths"];
   [encoder encodeObject:self.root_dir forKey:@"root_dir"];
-  [encoder encodeObject:self.total_size forKey:@"total_size"];
+  [encoder encodeObject:@(self.total_size) forKey:@"total_size"];
   [encoder encodeObject:self.asset_map forKey:@"asset_map"];
 }
 
@@ -61,7 +113,8 @@
 
 - (NSString*)description
 {
-  return [NSString stringWithFormat:@"<InfinitManagedFiles %p: %@>", self, self.uuid];
+  return [NSString stringWithFormat:@"<InfinitManagedFiles %p (%@): %@>",
+          self, self.uuid, self.managed_paths];
 }
 
 @end
