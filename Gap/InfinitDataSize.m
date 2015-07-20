561dc5996ed8b3e6d428f3e1418da85d314f06fd
Christopher Crone <christopher.crone@infinit.io>
2015-07-20 18:20:39 +0200

    DataSize: Use unbreakable space in data sizes. Only show significant figures that matter.

--- Gap/InfinitDataSize.m
+++ Gap/InfinitDataSize.m
@@ -10,24 +10,46 @@
 
 #import "InfinitGapLocalizedString.h"
 
+static dispatch_once_t _init_token = 0;
+static NSNumberFormatter* _formatter = nil;
+static double _kilo = 1.0f;
+static double _mega = 1.0f;
+static double _giga = 1.0f;
+static double _tera = 1.0f;
+static double _penta = 1.0f;
+
 @implementation InfinitDataSize
 
 // Do not confuse MB and MiB. Apple use MB, GB, etc for their file and storage sizes.
 + (NSString*)fileSizeStringFrom:(NSNumber*)file_size
 {
+  dispatch_once(&_init_token, ^
+  {
+    _formatter = [[NSNumberFormatter alloc] init];
+    _formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    _formatter.roundingMode = NSNumberFormatterRoundUp;
+    _formatter.maximumFractionDigits = 2;
+    _kilo = pow(10.0f, 3.0f);
+    _mega = pow(10.0f, 6.0f);
+    _giga = pow(10.0f, 9.0f);
+    _tera = pow(10.0f, 12.0f);
+    _penta = pow(10.0f, 15.0f);
+  });
   NSString* res = @"<nil>";
   double size = file_size.doubleValue;
   NSString* bytes = GapLocalizedString(@"B", @"bytes");
-  if (size < pow(10.0, 3.0))
-    res = [NSString stringWithFormat:@"%.0f %@", size, bytes];
-  else if (size < pow(10.0, 6.0))
-    res = [NSString stringWithFormat:@"%.0f K%@", size / pow(10.0, 3.0), bytes];
-  else if (size < pow(10.0, 9.0))
-    res = [NSString stringWithFormat:@"%.1f M%@", size / pow(10.0, 6.0), bytes];
-  else if (size < pow(10.0, 12.0))
-    res = [NSString stringWithFormat:@"%.2f G%@", size / pow(10.0, 9.0), bytes];
+  if (size < _kilo)
+    res = [NSString stringWithFormat:@"%@\u00A0%@", [_formatter stringFromNumber:@(size)], bytes];
+  else if (size < _mega)
+    res = [NSString stringWithFormat:@"%@\u00A0K%@", [_formatter stringFromNumber:@(size / _kilo)], bytes];
+  else if (size < _giga)
+    res = [NSString stringWithFormat:@"%@\u00A0M%@", [_formatter stringFromNumber:@(size / _mega)], bytes];
+  else if (size < _tera)
+    res = [NSString stringWithFormat:@"%@\u00A0G%@", [_formatter stringFromNumber:@(size / _giga)], bytes];
+  else if (size < _penta)
+    res = [NSString stringWithFormat:@"%@\u00A0T%@", [_formatter stringFromNumber:@(size / _tera)], bytes];
   else
-    res = [NSString stringWithFormat:@"%.3f T%@", size / pow(10.0, 12.0), bytes];
+    res = [NSString stringWithFormat:@"%@\u00A0P%@", [_formatter stringFromNumber:@(size / _penta)], bytes];
 
   return res;
 }
