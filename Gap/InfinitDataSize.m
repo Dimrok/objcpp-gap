91ccbab099c2c3f586b780665c413eabae343bea
Christopher Crone <christopher.crone@infinit.io>
2015-08-05 18:06:03 +0200

    DataSize: Ensure thread safety.

--- Gap/InfinitDataSize.m
+++ Gap/InfinitDataSize.m
@@ -18,10 +18,38 @@ static double _giga = 1.0f;
 static double _tera = 1.0f;
 static double _penta = 1.0f;
 
+static dispatch_queue_t _queue = nil;
+
 @implementation InfinitDataSize
 
 // Do not confuse MB and MiB. Apple use MB, GB, etc for their file and storage sizes.
 + (NSString*)fileSizeStringFrom:(NSNumber*)file_size
+{
+  [self _initialize];
+  __block NSString* res = @"<nil>";
+  dispatch_sync(_queue, ^
+  {
+    double size = file_size.doubleValue;
+    NSString* bytes = GapLocalizedString(@"B", @"bytes");
+    if (size < _kilo)
+      res = [NSString stringWithFormat:@"%@\u00A0%@", [_formatter stringFromNumber:@(size)], bytes];
+    else if (size < _mega)
+      res = [NSString stringWithFormat:@"%@\u00A0K%@", [_formatter stringFromNumber:@(size / _kilo)], bytes];
+    else if (size < _giga)
+      res = [NSString stringWithFormat:@"%@\u00A0M%@", [_formatter stringFromNumber:@(size / _mega)], bytes];
+    else if (size < _tera)
+      res = [NSString stringWithFormat:@"%@\u00A0G%@", [_formatter stringFromNumber:@(size / _giga)], bytes];
+    else if (size < _penta)
+      res = [NSString stringWithFormat:@"%@\u00A0T%@", [_formatter stringFromNumber:@(size / _tera)], bytes];
+    else
+      res = [NSString stringWithFormat:@"%@\u00A0P%@", [_formatter stringFromNumber:@(size / _penta)], bytes];
+  });
+  return res;
+}
+
+#pragma mark - Helpers
+
++ (void)_initialize
 {
   dispatch_once(&_init_token, ^
   {
@@ -34,24 +62,9 @@ static double _penta = 1.0f;
     _giga = pow(10.0f, 9.0f);
     _tera = pow(10.0f, 12.0f);
     _penta = pow(10.0f, 15.0f);
+    _queue = dispatch_queue_create("io.Infinit.DataSize", DISPATCH_QUEUE_SERIAL);
   });
-  NSString* res = @"<nil>";
-  double size = file_size.doubleValue;
-  NSString* bytes = GapLocalizedString(@"B", @"bytes");
-  if (size < _kilo)
-    res = [NSString stringWithFormat:@"%@\u00A0%@", [_formatter stringFromNumber:@(size)], bytes];
-  else if (size < _mega)
-    res = [NSString stringWithFormat:@"%@\u00A0K%@", [_formatter stringFromNumber:@(size / _kilo)], bytes];
-  else if (size < _giga)
-    res = [NSString stringWithFormat:@"%@\u00A0M%@", [_formatter stringFromNumber:@(size / _mega)], bytes];
-  else if (size < _tera)
-    res = [NSString stringWithFormat:@"%@\u00A0G%@", [_formatter stringFromNumber:@(size / _giga)], bytes];
-  else if (size < _penta)
-    res = [NSString stringWithFormat:@"%@\u00A0T%@", [_formatter stringFromNumber:@(size / _tera)], bytes];
-  else
-    res = [NSString stringWithFormat:@"%@\u00A0P%@", [_formatter stringFromNumber:@(size / _penta)], bytes];
 
-  return res;
 }
 
 @end
