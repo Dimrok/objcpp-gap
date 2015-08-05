57eb88aae0e8a13058735ed4809a75def80ac286
Christopher Crone <christopher.crone@infinit.io>
2015-08-05 18:04:24 +0200

    NSString+email: Ensure thread safety.

--- Gap/NSString+email.m
+++ Gap/NSString+email.m
@@ -9,6 +9,7 @@
 #import "NSString+email.h"
 
 static NSPredicate* _infinit_email_predicate = nil;
+static dispatch_once_t _infinit_email_predicate_token = 0;
 
 @implementation NSString (infinit_email)
 
@@ -16,9 +17,16 @@ static NSPredicate* _infinit_email_predicate = nil;
 {
   if (self.length == 0)
     return NO;
-  if (_infinit_email_predicate == nil)
+  return [[NSString _infinit_emailPredicate] evaluateWithObject:self];
+}
+
+#pragma mark - Helpers
+
++ (NSPredicate*)_infinit_emailPredicate
+{
+  dispatch_once(&_infinit_email_predicate_token, ^
   {
-    NSString* regex_str =
+    static NSString* regex_str =
       @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
       @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
       @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
@@ -27,8 +35,8 @@ static NSPredicate* _infinit_email_predicate = nil;
       @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
       @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     _infinit_email_predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES[c] %@", regex_str];
-  }
-  return [_infinit_email_predicate evaluateWithObject:self];
+  });
+  return _infinit_email_predicate;
 }
 
 @end
