d30bf9a3074bb5f22fa8d1a205712d7eb287b1a9
Christopher Crone <christopher.crone@infinit.io>
2015-08-05 18:05:06 +0200

    NSString+UUID: Ensure thread safety.

--- Gap/NSString+UUID.m
+++ Gap/NSString+UUID.m
@@ -10,24 +10,34 @@
 
 #define INFINIT_UUID_REGEX @"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 
-static NSRegularExpression* _infinit_regex = nil;
+static NSRegularExpression* _infinit_uuid_regex = nil;
+static dispatch_once_t _infinit_regex_token = 0;
 
 @implementation NSString (infinit_UUID)
 
 - (BOOL)infinit_isUUID
 {
-  if (_infinit_regex == nil)
-  {
-    _infinit_regex =
-      [NSRegularExpression regularExpressionWithPattern:INFINIT_UUID_REGEX
-                                                options:NSRegularExpressionCaseInsensitive
-                                                  error:nil];
-  }
   NSUInteger matches =
-    [_infinit_regex numberOfMatchesInString:self options:0 range:NSMakeRange(0, self.length)];
+    [[NSString  _infinit_uuidRegex] numberOfMatchesInString:self
+                                                    options:0
+                                                      range:NSMakeRange(0, self.length)];
   if (matches == 1)
     return YES;
   return NO;
+}
+
+#pragma mark - Helpers
+
++ (NSRegularExpression*)_infinit_uuidRegex
+{
+  dispatch_once(&_infinit_regex_token, ^
+  {
+    _infinit_uuid_regex =
+      [NSRegularExpression regularExpressionWithPattern:INFINIT_UUID_REGEX
+                                                options:NSRegularExpressionCaseInsensitive
+                                                  error:nil];
+  });
+  return _infinit_uuid_regex;
 }
 
 @end
