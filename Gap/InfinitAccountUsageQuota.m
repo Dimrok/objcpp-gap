1b4fce117052aa722814cbb3a9dd00f91b840ff4
Christopher Crone <christopher.crone@infinit.io>
2015-08-24 11:31:16 +0200

    AccountUsageQuota: Add isEqual.

--- Gap/InfinitAccountUsageQuota.m
+++ Gap/InfinitAccountUsageQuota.m
@@ -41,10 +41,22 @@
   return [[self alloc] _initWithUsage:usage quota:0 quotaSet:NO];
 }
 
+#pragma mark - NSObject
+
 - (NSString*)description
 {
   return [NSString stringWithFormat:@"Quota(usage: %@ / %@)",
           self.usage, self.quota ? self.quota : @"unlimited"];
+}
+
+- (BOOL)isEqual:(id)object
+{
+  if (![object isKindOfClass:self.class])
+    return NO;
+  InfinitAccountUsageQuota* other = (InfinitAccountUsageQuota*)object;
+  if ([other.quota isEqualToNumber:self.quota] && [other.usage isEqualToNumber:self.usage])
+    return YES;
+  return NO;
 }
 
 @end
